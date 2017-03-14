2e7a6e8a52f877e15e8a350bf43dfdb37c47fe35
Luciano Ramalho <luciano@ramalho.org>
2017-03-14 20:13:49 -0300

    função analisar linha ok

--- lib/alef.ex
+++ lib/alef.ex
@@ -4,15 +4,16 @@ defmodule Alef do
   """
 
   @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> Alef.hello
-      :world
-
+  analisar_linha/1
   """
-  def hello do
-    :world
+  def analisar_linha(linha) do
+    campos = String.split(linha, ";")
+    [codigo, nome] = Enum.slice(campos, 0..1)
+    palavras = nome <> " " <> Enum.at(campos, 10)
+            |> String.replace("-", " ")
+            |> String.split
+            |> MapSet.new
+    runa = <<String.to_integer(codigo, 16)::utf8>>
+    {runa, nome, palavras}
   end
 end
--- test/alef_test.exs
+++ test/alef_test.exs
@@ -2,7 +2,25 @@ defmodule AlefTest do
   use ExUnit.Case
   doctest Alef
 
-  test "the truth" do
-    assert 1 + 1 == 2
+  import Alef, only: [ analisar_linha: 1 ]
+
+  test "analisar linha simples de UnicodeData.txt" do
+    letra_A = "0041;LATIN CAPITAL LETTER A;Lu;0;L;;;;;N;;;;0061;"
+    palavras = MapSet.new(["A", "CAPITAL", "LATIN", "LETTER"])
+    assert {"A", "LATIN CAPITAL LETTER A", ^palavras} = analisar_linha(letra_A)
   end
+
+  test "analisar linha de UnicodeData.txt com hífen" do
+    hifen = "002D;HYPHEN-MINUS;Pd;0;ES;;;;;N;;;;;"
+    palavras = MapSet.new(["HYPHEN", "MINUS"])
+    assert {"-", "HYPHEN-MINUS", ^palavras} = analisar_linha(hifen)
+  end
+
+  test "analisar linha de UnicodeData.txt com hífen e campo 10" do
+    apostrofe = "0027;APOSTROPHE;Po;0;ON;;;;;N;APOSTROPHE-QUOTE;;;"
+    palavras = MapSet.new(["APOSTROPHE", "QUOTE"])
+    assert {"'", "APOSTROPHE", ^palavras} = analisar_linha(apostrofe)
+  end
+
+
 end
